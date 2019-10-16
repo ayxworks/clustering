60d7aaca653e0575b3882aac51b34995f4d0c2e3
Zuhaitz <32980658+Zuhaitz@users.noreply.github.com>
2019-10-16 18:26:48 +0200

    Código clustering
    Mas o menos ya esta solo falta probarlo cuando Alain haga su parte.

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -4,30 +4,79 @@ Created on Wed Oct 16 13:14:33 2019
 
 @author: StromValhalla
 """
-from numpy import mean
+"""from numpy import mean"""
 
 class Cluster:
     def __init__(self, vectores):
         self.vect = vectores
         self.clust = {}
         i = 1
-        while i<len(self.vect): self.clust[i] = self.vect[i-1]
+        while i<len(vectores): 
+            self.clust[f'C{i}'] = [vectores[i-1]]
+            i+=1
+        
+        self.dist = {}
+        self.dist[0] = self.clust
 
     
     def clustering (self):
-        centroides = {} 
+        iteracion = 0
         
-        for each in self.clust.keys():
-            centroide = self.calcularCentro(self.clust[each])
-            centroides[each] = centroide
+        while len(self.clust.keys()) != 1:
+            centroides = {} 
             
-        print(centroides)
+            for each in self.clust.keys():
+                print(self.clust[each])
+                centroide = self.calcularCentro(self.clust[each])
+                centroides[each] = centroide
+                
+            """Esta parte la tengo que cambiar, es solo provisional"""
+            cl1, cl2, dist = ('C1', 'C2', 1.7)
+    
+            vector1 = self.clust[cl1]
+            vector2 = self.clust[cl2]
+            
+            del self.clust[cl1]
+            del self.clust[cl2]
+            
+            self.clust[cl1] = vector1 + vector2
+            
+            self.dist[dist] = self.clust
+            iteracion+=1
+            
+            print(self.dist)
         
-    
-    
-    
+    """
+        Dado una lista de vectores te devuelve su centroide
+        pre: Una lista no vacia de tuplas numericas
+        post: Devuelve el centroide de esos vectores
+    """
     def calcularCentro (self, lista):
+        centro = []
+        i=0
+        x=0
+        
+        while i<len(lista[0]):
+            for each in lista: x+=each[i]
+            x=x/len(lista)
+            centro.append(x)
+            x=0
+            i+=1
+        
+        centro = tuple(centro)
+        return centro
+    
+    
+    
+    """Hacerlo manual? (Duda)"""
+    """def calcularCentro (self, lista):
         lista = tuple(lista)
         centroide = tuple(mean(lista, axis=0))
         
-        return centroide
+        return centroide"""
+        
+
+
+cl = Cluster([(1,2),(3,5),(1,3),(8,5)])
+
+cl.clustering()
