5a360e225c825a88f2f1fb03e3514eaee07efd4f
Alex Ye <ayxworks@gmail.com>
2019-10-21 20:01:18 +0200

    cambiar el tiempo de segundos con muchas decimas a mins y sec

--- Clustering/main.py
+++ Clustering/main.py
@@ -7,22 +7,25 @@ def main(argv):
     ##########################################################################################
     print('1: Preprocessing')
     documentos = preproceso.preprocesar()
-    #print(documentos.vector)
+    print(documentos.vector)
     #print(documentos.atributos)
-    print(documentos.tabla)
+    #print(documentos.tabla)
 
     tiempo = time.time() - comienzo
+    tiempo = time.strftime("%M:%S", time.gmtime(tiempo))
     print ('Ha tardado en preprocesar ', tiempo, 'segundos!')
     ##########################################################################################
     print('2: Clustering')
     #clustering.begin(fv)
 
     tiempo = time.time() - comienzo
+    tiempo = time.strftime("%M:%S", time.gmtime(tiempo))
     print ('Ha tardado en hacer el cluster jerarquico ', tiempo, 'segundos!')
     ##########################################################################################
 
 
     tiempo = time.time() - comienzo
+    tiempo = time.strftime("%M:%S", time.gmtime(tiempo))
     print ('Ha tardado en evaluar ', tiempo, 'segundos!')
 if __name__ == '__main__':
     main(sys.argv[1:])
