42bee6c2e1faacd8a03e20143b3104b06a69cc94
Zuhaitz <32980658+Zuhaitz@users.noreply.github.com>
2019-10-23 18:17:55 +0200

    Archivo con iteraciones y cambios
    Ahora guardo el texto de cada iteración en u archivo ademas de hacer cambios que hable con Alex y optimización

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -14,7 +14,7 @@ class Cluster:
         self.clust = {}
         i = 0
         while i<len(vectores): 
-            self.clust[i+1] = [vectores[i]]
+            self.clust[i+1] = [i]
             i+=1
         
         self.dist = {}
@@ -29,34 +29,62 @@ class Cluster:
     """
     def clustering (self):
         iteracion = 0
+        centroides = self.inicializarCentroides()
         
         while len(self.clust.keys()) != 1:
-            centroides = {} 
-            
             print(iteracion)
-            
-            for each in self.clust.keys():
-                centroide = ut.calcularCentro(self.clust[each])
-                centroides[each] = centroide
             
             print("Centroides calculados")
             cl1, cl2, dist = ut.minimaDistancia(centroides)
+            print("Distancia calculada")
             
-    
             vector1 = self.clust[cl1]
             vector2 = self.clust[cl2]
             
+            
             del self.clust[cl2]
+            del centroides[cl2]
             
             self.clust[cl1] = vector1 + vector2
+            vectores = self.sacarVetores(self.clust[cl1])
+            centroides[cl1] = ut.calcularCentro(vectores)
             
             self.dist[dist] = copy(self.clust)
+            
+            self.guardarIteracion(iteracion, dist, self.clust)
             
             iteracion+=1
             
         "print(self.dist)"
     
     
+    def inicializarCentroides(self):
+        centroides = {} 
+        
+        for each in self.clust.keys():
+            vectores = self.sacarVetores(self.clust[each])
+            centroide = ut.calcularCentro(vectores)
+            centroides[each] = centroide
+            
+        return centroides
+    
+    
+    
+    def sacarVetores(self, lista):
+        vectores = []
+        for num in lista:
+            vectores.append(self.vect[num])
+        
+        return vectores
+    
+    
+    def guardarIteracion(self, it, dist, clust):
+        string = ' Iteration={};  '.format(it)
+        string += 'Distancia={0:.2f};  '.format(round(dist, 2))
+        for cl in clust.keys(): string += '{};  '.format(clust[cl])
+        string += '\n' 
+        with open('resultados\iteraciones.txt', "a") as res: res.write(string)
+        
     
     """
     Guarda la estructura jerarquica
@@ -64,14 +92,16 @@ class Cluster:
     Post: El archivo con la estructura de datos guardada
     """
     def guardar(self, path):
-        with open(path, "wb") as res:   #Pickling
-            pickle.dump(self.dist, res)
+        with open(path, "wb") as res:
+            pickle.dump(self.dist, res)          
+            
 
 
-with open('resultados\datosAL.txt', "rb") as fp:  
-    clust = pickle.load(fp)
+"""with open('resultados\datosAL.txt', "rb") as fp:  
+    clust = pickle.load(fp)"""
 
-cl = Cluster(clust)
+cl = Cluster([(1,3),(1,4),(2,2),(5,2),(5,1),(7,2)])
+"cl = Cluster(clust)"
 
 cl.clustering() 
 print(cl.dist.keys())
